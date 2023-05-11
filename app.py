dfa17381cf26735a7a64045a1eada424317ba27a
aoqisun <793072943@qq.com>
2023-05-11 20:42:42 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask,url_for
+from flask import Flask, url_for, render_template
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+a = 'dd'
 app = Flask(__name__)
+
+
 @app.route('/user/<name>')
 def user_page(name):
     return "你是输入的User时{}：".format(name)
 
+
 @app.route('/')
-def hello():
+def index():
+    return render_template('index.html', name=name, movies=movies, a=a)
     # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-    return 'Hello'
+    # return 'Hello'
     # return '<h1>Hello Totoro!</h1><img src="https://img.soogif.com/NQy8FNhHUebORPnFRkJogMVARjCvkpRv.gif">'
+
 
 @app.route('/test')
 def test_url_for():
